f3fad58d55ea3e7ad716a3d97b2356e3d7cbd2e9
= <=>
2019-09-28 20:47:25 +0300

    allow users to create quotes by adding NewQuote component

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <form>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea><!--two way binding.-->
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+               <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button><!--@click.prevent, prevents default behavior. dont want to submitform tht will reload page-->
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                quote: '' //binding from text area
+            };
+        },
+        methods: {
+            createNew() { //emit new quote. connects NewQuote childto parent container. whatever you add when click button will be displayed in parent container too
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
